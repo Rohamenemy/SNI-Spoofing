8e9d7e07accc610160577037ce46737346abd682
RohamEnemy <117857907+Rohamenemy@users.noreply.github.com>
2026-05-25 00:55:35 +0330

    Update main.py

--- main.py
+++ main.py
@@ -6,27 +6,52 @@ import traceback
 import threading
 import json
 
-# from utils.proxy_protocols import parse_vless_protocol
 from utils.network_tools import get_default_interface_ipv4
 from utils.packet_templates import ClientHelloMaker
 from fake_tcp import FakeInjectiveConnection, FakeTcpInjector
 
+os.system('')
+
+class Colors:
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    CYAN = '\033[96m'
+    RED = '\033[91m'
+    RESET = '\033[0m'
+
+has_connected_once = False
 
 def get_exe_dir():
     """Returns the directory where the .exe (or script) is located."""
     if getattr(sys, 'frozen', False):
-        # Running as a PyInstaller EXE
         return os.path.dirname(sys.executable)
     else:
-        # Running as a normal Python script
         return os.path.dirname(os.path.abspath(__file__))
 
-
-# Build the path to config.json
 config_path = os.path.join(get_exe_dir(), 'config.json')
 
-# Load the config
-with open(config_path, 'r') as f:
+if not os.path.exists(config_path):
+    print(f"{Colors.YELLOW}File Config Peyda Nashod Eshkal Nadare Yeki Misazim{Colors.RESET}")
+    
+    user_ip = input(f"{Colors.CYAN}Connect IP Ro vared Konid (:  {Colors.RESET}").strip()
+    connect_ip = user_ip if user_ip else "188.114.98.0"
+    
+    user_sni = input(f"{Colors.CYAN} Lotfan FakeSNI Ro Vared Koned : {Colors.RESET}").strip()
+    fake_sni = user_sni if user_sni else "auth.vercel.com"
+    
+    default_config = {
+        "LISTEN_HOST": "0.0.0.0",
+        "LISTEN_PORT": 40443,
+        "CONNECT_IP": connect_ip,
+        "CONNECT_PORT": 443,
+        "FAKE_SNI": fake_sni
+    }
+    
+    with open(config_path, 'w', encoding='utf-8') as f:
+        json.dump(default_config, f, indent=4)
+    print(f"{Colors.GREEN}[+] File Config.json Add Shod!{Colors.RESET}\n")
+
+with open(config_path, 'r', encoding='utf-8') as f:
     config = json.load(f)
 
 LISTEN_HOST = config["LISTEN_HOST"]
@@ -38,10 +63,7 @@ INTERFACE_IPV4 = get_default_interface_ipv4(CONNECT_IP)
 DATA_MODE = "tls"
 BYPASS_METHOD = "wrong_seq"
 
-##################
-
 fake_injective_connections: dict[tuple, FakeInjectiveConnection] = {}
-
 
 async def relay_main_loop(sock_1: socket.socket, sock_2: socket.socket, peer_task: asyncio.Task,
                           first_prefix_data: bytes):
@@ -59,74 +81,36 @@ async def relay_main_loop(sock_1: socket.socket, sock_2: socket.socket, peer_tas
                 if sent_len != len(data):
                     raise ValueError("incomplete send")
             except Exception:
-                sock_1.close()
-                sock_2.close()
                 peer_task.cancel()
+                try:
+                    await asyncio.sleep(0.01)
+                except asyncio.CancelledError:
+                    pass
+                
+                try:
+                    sock_1.close()
+                except Exception:
+                    pass
+                try:
+                    sock_2.close()
+                except Exception:
+                    pass
                 return
     except Exception:
-        traceback.print_exc()
-        sys.exit("relay main loop error!")
+        pass
 
 
 async def handle(incoming_sock: socket.socket, incoming_remote_addr):
+    global has_connected_once
     try:
         loop = asyncio.get_running_loop()
-        # try:
-        #     data = await loop.sock_recv(incoming_sock, 65575)
-        #     if not data:
-        #         raise ValueError("eof")
-        # except Exception:
-        #     incoming_sock.close()
-        #     return
-        # try:
-        #     version, uuid_bytes, transport_protocol, remote_address_type, remote_address, remote_port, payload_index = parse_vless_protocol(
-        #         data)
-        # except Exception as e:
-        #     print("No Vless Request!, Connection Closed", repr(e), data)
-        #     incoming_sock.close()
-        #     return
-        # if transport_protocol != "tcp":
-        #     print("Transport Protocol Error!, Connection Closed", transport_protocol, data)
-        #     incoming_sock.close()
-        #     return
-        # if remote_address_type == "hostname":
-        #     print("hostname address not implemented yet!", data)
-        #     incoming_sock.close()
-        #     return
-        # if remote_address_type == "ipv4":
-        #     if not INTERFACE_IPV4:
-        #         print("no interface ipv4!", data)
-        #         incoming_sock.close()
-        #         return
-        #     family = socket.AF_INET
-        #     src_ip = INTERFACE_IPV4
-        #
-        # elif remote_address_type == "ipv6":
-        #     if not INTERFACE_IPV6:
-        #         print("no interface ipv6!", data)
-        #         incoming_sock.close()
-        #         return
-        #     family = socket.AF_INET6
-        #     src_ip = INTERFACE_IPV6
-        #
-        # else:
-        #     print(data)
-        #     sys.exit("impossible address type!")
-
-        # try:
-        #     fake_sni_host, data_mode, bypass_method = UUID_FAKE_MAP[uuid_bytes]
-        # except KeyError:
-        #     print("unmatched uuid", uuid_bytes)
-        #     incoming_sock.close()
-        #     return
-
-        # if data_mode == "http":
-        #     ...
+        
         if DATA_MODE == "tls":
             fake_data = ClientHelloMaker.get_client_hello_with(os.urandom(32), os.urandom(32), FAKE_SNI,
                                                                os.urandom(32))
         else:
-            sys.exit("impossible mode!")
+            sys.exit(f"{Colors.RED}impossible mode!{Colors.RESET}")
+            
         outgoing_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         outgoing_sock.setblocking(False)
         outgoing_sock.bind((INTERFACE_IPV4, 0))
@@ -135,10 +119,12 @@ async def handle(incoming_sock: socket.socket, incoming_remote_addr):
         outgoing_sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, 2)
         outgoing_sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, 3)
         src_port = outgoing_sock.getsockname()[1]
+        
         fake_injective_conn = FakeInjectiveConnection(outgoing_sock, INTERFACE_IPV4, CONNECT_IP, src_port, CONNECT_PORT,
                                                       fake_data,
                                                       BYPASS_METHOD, incoming_sock)
         fake_injective_connections[fake_injective_conn.id] = fake_injective_conn
+        
         try:
             await loop.sock_connect(outgoing_sock, (CONNECT_IP, CONNECT_PORT))
         except Exception:
@@ -148,50 +134,37 @@ async def handle(incoming_sock: socket.socket, incoming_remote_addr):
             incoming_sock.close()
             return
 
-        # if bypass_method == "wrong_checksum":
-        #     ...
-
         if BYPASS_METHOD == "wrong_seq":
             try:
                 await asyncio.wait_for(fake_injective_conn.t2a_event.wait(), 2)
                 if fake_injective_conn.t2a_msg == "unexpected_close":
                     raise ValueError("unexpected close")
                 if fake_injective_conn.t2a_msg == "fake_data_ack_recv":
-                    pass
+                    if not has_connected_once:
+                        print(f"{Colors.GREEN}[+] Connect: Tunnel to {CONNECT_IP}:{CONNECT_PORT} established.{Colors.RESET}")
+                        has_connected_once = True
                 else:
-                    sys.exit("impossible t2a msg!")
+                    sys.exit(f"{Colors.RED}impossible t2a msg!{Colors.RESET}")
             except Exception:
                 fake_injective_conn.monitor = False
-                del fake_injective_connections[fake_injective_conn.id]
+                if fake_injective_conn.id in fake_injective_connections:
+                    del fake_injective_connections[fake_injective_conn.id]
                 outgoing_sock.close()
                 incoming_sock.close()
                 return
         else:
-            sys.exit("unknown bypass method!")
+            sys.exit(f"{Colors.RED}unknown bypass method!{Colors.RESET}")
 
         fake_injective_conn.monitor = False
-        del fake_injective_connections[fake_injective_conn.id]
-
-        # early_data = data[payload_index:]
-        # if early_data:
-        #     try:
-        #         sent_len = await loop.sock_sendall(outgoing_sock, early_data)
-        #         if sent_len != len(early_data):
-        #             raise ValueError("incomplete send")
-        #     except Exception:
-        #         outgoing_sock.close()
-        #         incoming_sock.close()
-        #         return
+        if fake_injective_conn.id in fake_injective_connections:
+            del fake_injective_connections[fake_injective_conn.id]
 
         oti_task = asyncio.create_task(
-            relay_main_loop(outgoing_sock, incoming_sock, asyncio.current_task(), b""))  # bytes([version, 0])
+            relay_main_loop(outgoing_sock, incoming_sock, asyncio.current_task(), b""))
         await relay_main_loop(incoming_sock, outgoing_sock, oti_task, b"")
 
-
-
     except Exception:
-        traceback.print_exc()
-        sys.exit("handle should not raise exception")
+        pass
 
 
 async def main():
@@ -205,6 +178,9 @@ async def main():
     mother_sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, 3)
     mother_sock.listen()
     loop = asyncio.get_running_loop()
+    
+    print(f"{Colors.CYAN}Listening on {LISTEN_HOST}:{LISTEN_PORT}...{Colors.RESET}")
+    
     while True:
         incoming_sock, addr = await loop.sock_accept(mother_sock)
         incoming_sock.setblocking(False)
@@ -219,10 +195,11 @@ if __name__ == "__main__":
     w_filter = "tcp and " + "(" + "(ip.SrcAddr == " + INTERFACE_IPV4 + " and ip.DstAddr == " + CONNECT_IP + ")" + " or " + "(ip.SrcAddr == " + CONNECT_IP + " and ip.DstAddr == " + INTERFACE_IPV4 + ")" + ")"
     fake_tcp_injector = FakeTcpInjector(w_filter, fake_injective_connections)
     threading.Thread(target=fake_tcp_injector.run, args=(), daemon=True).start()
-    print("هشن شومافر تیامح دینکیم هدافتسا دازآ تنرتنیا هب یسرتسد یارب همانرب نیا زا رگا")
-    print(
-        "دراد امش تیامح هب زاین هک مراد رظن رد دازآ تنرتنیا هب ناریا مدرم مامت یسرتسد یارب یدایز یاه همانرب و اه هژورپ")
-    print("\n")
-    print("USDT (BEP20): 0x76a768B53Ca77B43086946315f0BDF21156bF424\n")
-    print("@patterniha")
-    asyncio.run(main())
+
+    print(f"@RohamEnemy{Colors.RESET}\n")
+    
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print(f"\n{Colors.YELLOW}by (:{Colors.RESET}")
+        sys.exit(0)
